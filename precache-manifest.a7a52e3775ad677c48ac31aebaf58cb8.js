a6416895490bdaa4e0564a1e082d73bdc3b854fe
Muralidhar <muralidharreddy80@gmail.com>
2020-05-06 22:15:30 +0530

    Updates

---
+++ precache-manifest.a7a52e3775ad677c48ac31aebaf58cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178a01fa25473ae2ce46247232ea1025",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/index.html"
+  },
+  {
+    "revision": "9682ad664d2235a70374",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/static/css/main.4fd886da.chunk.css"
+  },
+  {
+    "revision": "03abef63f86618bbe5bf",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/static/js/2.b16775e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/static/js/2.b16775e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9682ad664d2235a70374",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/static/js/main.962a630e.chunk.js"
+  },
+  {
+    "revision": "3a61ec6f971287e8de74",
+    "url": "/muralidharreddy0703/React_login_page_with_state_management/static/js/runtime-main.9e4170e6.js"
+  }
+]);
